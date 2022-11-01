7379e45b210c2e5d212d9ab939a1358877cb1ced
Joseph Chamochumbi <sephxd1234@gmail.com>
2022-11-01 18:33:42 +0100

    refactor: Rename main view controller to MemeViewController

--- MemeMe/MemeViewController.swift
+++ MemeMe/MemeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var topTextField: MemeUITextField!
     @IBOutlet var bottomTextField: MemeUITextField!
     
